c312ae9af13aa68858bb7df5cb004b09451f4972
Ioannis Tsampras <82767099+Skorpinakos@users.noreply.github.com>
2022-11-10 05:32:56 +0200

    all nice and tidy

--- Edge Compute/prototype for plant finder.py
+++ Edge Compute/prototype for plant finder.py
@@ -2,7 +2,7 @@
 import time
 from clustering_utils import check_cluster_multitudes,find_elbow,find_centroids,plot_cluster_graph,plot_image_with_centers
 from image_utils import detect_edges,edge_image_to_edge_points_np_list
-
+from matplotlib import pyplot as plt
 
 
 filename='Capture1.png' #set image to test
@@ -14,13 +14,13 @@ edge_point_list_simple=edge_image_to_edge_points_np_list(edge_image_simple) #tak
 test_cases=range(2,22) #set the possible cases for plant multitude (the algorithm will check for each number and find the best match, use as smaller range as possible and preferably weighted to the left to improve performance)
 t3=time.time()
 wcss=check_cluster_multitudes(test_cases,edge_point_list_simple) #gets error list from trying all possible test cases
-plot_cluster_graph(test_cases, wcss) #plots error list to get idea of elbo graph
+#plot_cluster_graph(test_cases, wcss) #plots error list to get idea of elbo graph
 t4=time.time()
 n=find_elbow(wcss,test_cases) #finds where the elbow occurs in the error graph 
 t5=time.time()
 centers_x,centers_y=find_centroids(edge_point_list_good,n) #gets coordiantes of plant centers after running the algorithm once again for the correct n (could have stored data from the test cases run but one run doesn't cost as much, also this time we use more detail in the execution for optimal results and not just error estimation)
 t6=time.time()
-plot_image_with_centers(img,centers_x,centers_y) #show image
+#plot_image_with_centers(img,centers_x,centers_y) #show image
 
 print('time for edge filter:              ',t2-t1)
 print('time to find errors for test cases:',t4-t3)
