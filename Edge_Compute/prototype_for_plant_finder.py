09d6597cf64ef812433f9ff148d84a88f5e68e70
Ioannis Tsampras <gtsambras@gmail.com>
2022-11-24 02:53:42 +0200

    minor name change

--- Edge_Compute/prototype_for_plant_finder.py
+++ Edge_Compute/prototype_for_plant_finder.py
@@ -1,6 +1,6 @@
 
 import time
-from clustering_utils import check_cluster_multitudes,find_elbow,find_centroids,plot_cluster_graph,plot_image_with_centers_lines,find_lines
+from s_clustering_utils import check_cluster_multitudes,find_elbow,find_centroids,plot_cluster_graph,plot_image_with_centers_lines,find_lines
 from image_utils import detect_edges,edge_image_to_edge_points_np_list
 
 path="Edge_Compute/images/"
