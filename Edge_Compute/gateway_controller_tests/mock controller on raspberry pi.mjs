e8ca0c929574781b6d8110f6204c07a2d8071aa3
Skorpinakos <gtsambras@gmail.com>
2022-12-23 21:53:18 +0200

    refactoring dirs starting final dev for rasp ctrl

--- Edge_Compute/gateway_controller_tests/mock controller on raspberry pi.mjs
+++ Edge_Compute/gateway_controller_tests/mock controller on raspberry pi.mjs
@@ -9,22 +9,14 @@ const router = express.Router(); //make a router object
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////// backend functions
 let give_back = function(req,res){
-        res.json({'answer':100})
+        //res.json({'answer':100});
+        res.sendFile("image.png",{ root:"."});
+        console.log('sent image')
       };
 
-
-let update_center =async function(req,res){
-        const response = await fetch('http://localhost:3000');
-        const data = await response.json();
-        
-        //console.log(data);
-        res.json(data);
-      
-        };
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////// express routes
 app.use(router);
 router.route('/').get(give_back);
-router.route('/update').get(give_back);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////// initializing server
 app.listen(port, () => console.log(`App listening to port ${port}`));
